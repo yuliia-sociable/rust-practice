514243f26f8ad1734e6f482acff3d1b685782c9d
yuliia-sociable <yukozoriz@e-u.edu.ua>
2025-03-16 23:41:18 +0200

    Update hw06.rs

---
+++ src/homework/hw06.rs
@@ -0,0 +1,20 @@
+fn draw_tree(triangles: usize) {
+    // Додаємо нульовий рівень через ітератор
+    (0..1).for_each(|_| {
+        let padding = " ".repeat(triangles * 2);
+        println!("{}*", padding);
+    });
+    
+    (0..triangles).for_each(|t| {
+        (0..(t + 2)).for_each(|i| {
+            let stars = (0..(2 * i + 1)).map(|_| "*").collect::<String>();
+            let padding = " ".repeat(triangles * 2 - i);
+            println!("{}{}", padding, stars);
+        });
+    });
+}
+
+fn main() {
+    let triangles = 5; // Кількість трикутників, можна змінити
+    draw_tree(triangles);
+}
